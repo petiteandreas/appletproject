bcd6b6a9a12321bd94c37930cfe133f787987782
petiteandreas <andrii.d.nikitin@gmail.com>
2015-07-07 10:54:34 +0300

    DocumentCsvParser completed. TODO - cover it with tests

--- AppletProject/src/main/java/com/andriidnikitin/appletapp/dao/DocumentCsvParser.java
+++ AppletProject/src/main/java/com/andriidnikitin/appletapp/dao/DocumentCsvParser.java
@@ -26,27 +26,36 @@ public class DocumentCsvParser {
 		return result.toString();
 	}
 	
-	public static String parseCsvString(Document doc){
-		StringBuilder result = new StringBuilder();
-		String comma = ",";
-
-		result.append(doc.getSurname()).append(comma);
-		result.append(doc.getName()).append(comma);
-		result.append(doc.getPatronym()).append(comma);
-		result.append(unparseDate(doc.getBirthday())).append(comma);
-		result.append(doc.getBirthplaceCity()).append(comma);
-		result.append(doc.getBirthplaceArea()).append(comma);
-		result.append(doc.getBirthplaceRegion()).append(comma);
-		result.append(doc.getPassportSerial()).append(comma);
-		result.append(doc.getPassportId()).append(comma);
-		result.append(doc.getRegistrator()).append(comma);
-		result.append(doc.getRegistratorDepartment()).append(comma);
-		result.append(unparseDate(doc.getDateOfRegistrating()));
+	public static Document parseCsvString(String str) throws AppletProjectInvalidFileException{
 		
-		return result.toString();
+		Document doc = new  Document();		
+		String[] array = str.split(",");
+		int i = 0;
+		try{
+			doc.setSurname(array[i++]);
+			doc.setName(array[i++]);
+			doc.setPatronym(array[i++]);
+			doc.setBirthday(parseDate(array[i++]));
+			doc.setBirthplaceCity(array[i++]);
+			doc.setBirthplaceArea(array[i++]);
+			doc.setBirthplaceRegion(array[i++]);
+			doc.setPassportSerial(array[i++]);
+			doc.setPassportId(array[i++]);
+			doc.setRegistrator(array[i++]);
+			doc.setRegistratorDepartment(array[i++]);
+			doc.setDateOfRegistrating(parseDate(array[i++]));
+			
+		} catch (IndexOutOfBoundsException e){
+			throw new AppletProjectInvalidFileException(e);
+		}
+		
+		return doc;
 	}
 	
 	private static String unparseDate(Date d){
+		if (d==null){
+			throw new IllegalArgumentException("Date cannot be null."); 
+		}
 		String str = d.toString().replace(',', '@');
 		return str;
 	}
